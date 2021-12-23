b74d227d1b1b6d37477d96fc24e5836fed14c864
typaky <93198855+typaky@users.noreply.github.com>
2021-12-23 22:31:22 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,20 +1,12 @@
-
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import { Route } from "react-router-dom";
-import MainPage from "./components/MainPage";
-import SignInPage from "./components/SignInPage";
-import ProfilePage from "./components/ProfilePage";
-import PrivatRoute from "./components/PrivatRoute";
 
-import "./styles.css";
+import App from "./App";
 
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <BrowserRouter>
-    <Route exact path="/" component={MainPage} />
-    <Route path="/sign-in" component={SignInPage} />
-    <PrivatRoute path="/profile" component={ProfilePage} />
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  rootElement
+)
